db51e8a39f7251ef59fe6cc9a131422c7242a8de
cabralrobert <robertcabral@alu.ufc.br>
2016-06-13 12:11:40 -0300

    Inicial

---
+++ gpioClk.h
@@ -0,0 +1,14 @@
+#ifndef GPIOCLK_H_
+#define GPIOCLK_H_
+
+#include "gpioLED.h"
+#include "gpioFunctions.h"
+
+
+void GPIO3_ModuleClkConfig(void);
+void GPIO2_ModuleClkConfig(void);
+void GPIO1_ModuleClkConfig(void);
+void GPIO0_ModuleClkConfig(void);
+void gpioModuleClk(int moduleClk);
+
+#endif //GPIOCLK_H_
---
+++ src/gpioRobert.c
@@ -0,0 +1,20 @@
+/*******************************************************
+*                                                      *
+*   Criado em: Sáb 11/Jun/2016,  13:48 hs              *
+*   Instituicao: Universidade Federal do Ceará         *
+*   Autor: Robert Cabral                               *
+*   email: robertcabral@alu.ufc.br                     *
+*                                                      *
+********************************************************/
+#include "gpioFunctions.h"
+#include "gpioLED.h"
+
+int main(){
+
+	ledInit(23, 1);
+
+	return(0);
+
+}
+
+
